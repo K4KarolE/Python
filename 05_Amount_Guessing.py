3f1a75b6004498c2c1f58d516f4419ee68ec0517
K4KarolE <karoly.egyed@protonmail.com>
2022-05-13 06:30:04 +0100

    #1

---
+++ 05_Amount_Guessing.py
@@ -0,0 +1,14 @@
+# Amount Guessing Game
+
+import random
+
+print()
+answer = input('Are you ready for playing?\n')
+
+if answer.lower() != 'yes':
+    quit()
+
+print()
+print('Guess the amount of characters!\n')
+
+print('$' * 300)
