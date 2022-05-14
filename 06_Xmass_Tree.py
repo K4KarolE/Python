ca65fb8cc1ec935c80c33a6e0f68c7b32746898c
K4KarolE <karoly.egyed@protonmail.com>
2022-05-14 14:21:37 +0100

    ready

---
+++ 06_Xmass_Tree.py
@@ -0,0 +1,43 @@
+x=0
+while True:
+    q = '!' * (1+x)
+    p = q.center(100)
+    print(p)
+    if x < 15:
+        x += 4
+        continue
+    break
+
+x=3
+while True:
+    q = '!' * (1+x)
+    p = q.center(100)
+    print(p)
+    if x < 25:
+        x += 4
+        continue
+    break
+
+x=3
+while True:
+    q = '!' * (1+x)
+    p = q.center(100)
+    print(p)
+    if x < 34:
+        x += 6
+        continue
+    break
+
+x=1
+while True:
+    q = '!' * 4
+    p = q.center(100)
+    print(p)
+    if x < 5:
+        x += 1
+        continue
+    break
+
+q = '!' * 100
+p = q.center(100)
+print(p)
