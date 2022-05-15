47d7c4f909df7317369a1119c87a3a89c2d05f57
K4KarolE <karoly.egyed@protonmail.com>
2022-05-15 19:22:54 +0100

    ready

---
+++ 07_01_Pyautogui_Monitor_Extend.py
@@ -0,0 +1,15 @@
+import pyautogui
+
+with pyautogui.hold('win'):
+        pyautogui.press(['p'], interval=0.3)
+
+pyautogui.press('p')
+pyautogui.press('p')
+pyautogui.press('p')
+pyautogui.press('enter')
+
+
+
+
+
+
---
+++ 07_02_Pyautogui_Monitor_2nd_screen_only.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+with pyautogui.hold('win'):
+        pyautogui.press(['p'], interval=0.3)
+
+pyautogui.press('p')
+pyautogui.press('enter')
