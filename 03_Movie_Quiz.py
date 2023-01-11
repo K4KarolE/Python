f94ed79dbf7be4e4eb1e0aa839c0d6666fbdc0f0
Alexey Kudelko <flexivanov237@gmail.com>
2023-01-11 11:16:48 +0300

    randomizer added, quantity of code reduced

--- 03_Movie_Quiz.py
+++ 03_Movie_Quiz.py
@@ -1,67 +1,44 @@
 # Movie Quiz Game
 
-print("")
-introq = input("Is it a lovely day for a little Movie Quiz? " )
+import random, time
 
-if introq.lower() != "yes":
-      quit()
+questions_list = ['Which one is the best "planet" to celabrate Mother`s day on?',
+                  'Who is the father of Luke?',
+                  'What should Neo follow?',
+                  'What is the ring carrier`s profession?',
+                  'Who is the sexiest fictional character?' ]
 
-print("")
-print("Hurray! Let`s play!\n")
+options_list = ['A.The Sun, B.Ton 618(black hole), C.LV-426 ',
+                'A.Leia, B.Darth Vader, C.Jar Jar ',
+                'A.A white rabbit, B.A yellow submarine, C.A pink panther ',
+                'A.Be a nephew, B.Be a hobbit, C.Spartans: Auhh Auhh Auhh  ',
+                'A.Gimli, B.Jabba the Hutt, C.Jessica Rabbit ']
+
+answers_list = ['C', 'B', 'A', 'B', 'C']
+
 score = 0
+def question(questions_list, options_list, answers_list):
+    random_number = questions_list.index(random.choice(questions_list))
+    print(questions_list[random_number])
+    answer = input(options_list[random_number])
+    if answer.upper() == answers_list[random_number]:
+        print('Correct')
+        choice = 1
+    else:
+        print('Incorrect!')
+        choice = 0
+    questions_list.pop(random_number)
+    answers_list.pop(random_number)
+    options_list.pop(random_number)
+    return choice
 
-
-# 1st Question
-print('Which one is the best "planet" to celabrate Mother`s day on?')
-answer = input('A.The Sun, B.Ton 618(black hole), C.LV-426 ')
-if answer.upper() != 'C':
-    print('Incorrect')
-else:
-    print('Correct!')
-    score += 1
-print("")
-
-# 2nd Question
-print('Who is the father of Luke?')
-answer = input('A.Leia, B.Darth Vader, C.Jar Jar ')
-if answer.upper() != 'B':
-    print('Incorrect')
-else:
-    print('Correct!')
-    score += 1
-print("")
-
-# 3rd Question
-print('What should Neo follow?')
-answer = input('A.A white rabbit, B.A yellow submarine, C.A pink panther ')
-if answer.upper() != 'A':
-    print('Incorrect')
-else:
-    print('Correct!')
-    score += 1
-print("")
-
-# 4th Question
-print('What is the ring carrier`s profession?')
-answer = input('A.Be a nephew, B.Be a hobbit, C.Spartans: Auhh Auhh Auhh  ')
-if answer.upper() != 'B':
-    print('Incorrect')
-else:
-    print('Correct!')
-    score += 1
-print("")
-
-# 5th Question
-print('Who is the sexiest fictional character?')
-answer = input('A.Gimli, B.Jabba the Hutt, C.Jessica Rabbit ')
-if answer.upper() != 'C':
-    print('Incorrect')
-else:
-    print('Correct!')
-    score += 1
-print("")
+for i in range (1,6):
+    if question(questions_list, options_list, answers_list) == 1:
+        score += 1
 
 
 print('Your got ' + str(score) + ' answers right! (' + str(score/5*100) + '%)\n')
 
 print("Thx for playing!\n")
+
+time.sleep(5)
